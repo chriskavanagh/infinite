a83cb7a0c28a79f8a7869122386d3283986fcceb
Chris Kavanagh <ckava3@gmail.com>
2022-01-04 08:04:49 -0500

    crap

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const cards = document.querySelectorAll(".card");
+const container = document.querySelector(".box-container");
+const box = document.querySelector(".box");
+const t = document.querySelector(".text");
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -14,3 +17,12 @@ const observer = new IntersectionObserver(
 cards.forEach((card) => {
   observer.observe(card);
 });
+
+const boxObserver = new ResizeObserver((entries) => {
+  const boxElement = entries[0];
+  const isSmall = boxElement.contentRect.width < 650;
+  boxElement.target.style.fontSize = isSmall ? "30px" : "15px";
+  boxElement.target.style.color = isSmall ? "white" : "white";
+});
+
+boxObserver.observe(box);
